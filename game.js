ccff352ba0a7384145ff35489d157fdfd7e5e30a
Svetlana Sokolova <Svetlana.Sokolova@gmail.com>
2018-06-18 15:00:28 -0700

    additing var count

--- game.js
+++ game.js
@@ -1,17 +1,17 @@
 var correctNumber = 12;
-won = False;
-for (i = 1; i <= 5; i++) {
+won = false;
+count = 5;
+for (i = 1; i <= count; i++) {
     var guessingNumber = window.prompt("Guess number");
     if (guessingNumber === correctNumber) {
         document.write("<p>Your guess is correct!</p>");
-        won = True;
+        won = true;
     break;
      }
     else {
          window.alert("Sorry, try again, you have " + (5 - i) + " attempt");
-         if (won == False) {
-             window.alert("Sorry, try later again");  
-         }
-
      }
+}
+if (won == false) {
+    window.alert("<p>Game over ;(</p>");  
 }
