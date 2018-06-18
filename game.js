8563f8ea7ed55a6f14c3af5ee07e5ffe2ed83eca
Svetlana Sokolova <Svetlana.Sokolova@gmail.com>
2018-06-18 14:45:41 -0700

    for

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
 var correctNumber = 12;
 
 for (i = 1; i <= 5; i++) {
-var guessingNumber = window.prompt("Guess number");
-if (guessingNumber === correctNumber) {
-    document.write("<p>Your guess is correct!</p>");
+    var guessingNumber = window.prompt("Guess number");
+    if (guessingNumber === correctNumber) {
+        document.write("<p>Your guess is correct!</p>");
     break;
-}
-else {
-     window.alert("Sorry, try again, you have " + (5 - i) + " attempt");
+     }
+    else {
+         window.alert("Sorry, try again, you have " + (5 - i) + " attempt");
      }
 }
