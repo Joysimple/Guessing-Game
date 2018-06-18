d8da57b871f8f22b7949ba127ac380773f732484
Svetlana Sokolova <Svetlana.Sokolova@gmail.com>
2018-06-18 14:33:19 -0700

    additing for

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 var correctNumber = 12;
 var guessingNumber = window.prompt("Guess number");
+for (i = 1; i <= 5; i++) {
 if (guessingNumber === correctNumber) {
     document.write("<p>Your guess is correct!</p>");
 }
 else {
-    window.alert("Sorry, try again");
+     window.alert("Sorry, try again, you have " + (5 - i) + " attempt");
+     }
 }
