4b03ce47f7d7f90e1ff966e337b68c16d7423fdf
Svetlana Sokolova <Svetlana.Sokolova@gmail.com>
2018-06-19 13:22:55 -0700

    changing {} position

--- game.js
+++ game.js
@@ -8,29 +8,27 @@ function isPreviousGuess(guessingNumber){
     return duplicate;
 }
 
- while(i <= count) {
-     
+while(i <= count) {
     var guessingNumber = window.prompt("Guess number");
     if (guessingNumber == correctNumber) {
         document.write("<p>Your guess is correct!</p>");
         won = true;
         break;
-     }
+    }
     else {
-         var a = isPreviousGuess(guessingNumber); 
-         if (a == -1){
-             guessingArray[i] = guessingNumber;
-             window.alert("Sorry, try again, you have guessed: " + guessingArray.toString() + ", you have " + (count - i) + " attempt");
-         
-         }
-         else{
+        var a = isPreviousGuess(guessingNumber); 
+        if (a == -1) {
+            guessingArray[i] = guessingNumber;
+            window.alert("Sorry, try again, you have guessed: " + guessingArray.toString() + ", you have " + (count - i) + " attempt");
+        }
+        else {
             window.alert("Sorry, you already guessed this number, try again"); 
             continue;
-         }
+        }
 
-             }
-             i++;
-         }
+    }
+i++;
+}
      
 if (won == false) {
     window.alert("<p>Game over ;(</p>");  
